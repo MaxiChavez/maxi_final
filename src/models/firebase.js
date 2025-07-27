f38c8b89a01b46f80f2857dea1b9f96fee3c4c03
Maximiliano Chavez <maxi.chavez.aitaca@gmail.com>
2025-07-27 22:57:44 +0200

    clean code

--- src/models/firebase.js
+++ src/models/firebase.js
@@ -26,33 +26,3 @@ console.log("FIREBASE CONFIG DEBUG:", {
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
 });
-
-// /////
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDT9dd9p4IAcV92dyATvtXnTP_8zbReYmQ",
-//   authDomain: "entrega-final-maxi.firebaseapp.com",
-//   projectId: "entrega-final-maxi",
-//   storageBucket: "entrega-final-maxi.firebasestorage.app",
-//   messagingSenderId: "58293353599",
-//   appId: "1:58293353599:web:cd4959fdb852664c77d34a",
-//   measurementId: "G-1T918KLTJR",
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-// // Initialize Firestore
-// const db = getFirestore(app);
-
-// export { db };
